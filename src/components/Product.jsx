a2718aae27dbce8d79616c4112491eaf716b9122
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2024-09-18 14:19:45 -0700

    fixing image missing problem

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,19 @@
-export default function Product(product) {
-
-    return(
-        <div class="product">
-            <div className="product-name">
-            <img src={product.image_url} alt="" />
-            </div>
-            <p>
-              <span class="pr-name">{product.name}</span>
-              <span class="pr-price">${product.price}</span>
-            </p>
-            <button>add to cart</button>
-            <button>
-                <span>&#129293;</span>
-                <span>add to wishlist</span>
-            </button>
-          </div>
+export default function Product({ product }) {
+    return (
+      <div className="product">
+        <div className="product-name">
+          <img src={product.image_url} alt={product.name} />
+        </div>
+        <p>
+          <span className="pr-name">{product.name}</span>
+          <span className="pr-price">${product.price}</span>
+        </p>
+        <button>add to cart</button>
+        <button>
+          <span>&#129293;</span>
+          <span>add to wishlist</span>
+        </button>
+      </div>
     );
-}
+  }
+  
